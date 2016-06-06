1e9a54fda715585008b16c7109c5148a3ec512c5
alvin <494673006@qq.com>
2016-06-06 17:55:49 +0800

    2016年6月6日17:55:41

--- videoplayer/src/main/java/app/yongjiasoftware/com/videoplayer/ui/activity/MusicPlayerActivity.java
+++ videoplayer/src/main/java/app/yongjiasoftware/com/videoplayer/ui/activity/MusicPlayerActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
